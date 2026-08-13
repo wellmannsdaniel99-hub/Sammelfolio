232037bd55421ce99bd248b1319bd57eb0845314
wellmannsdaniel99-hub <wellmanns.daniel99@gmail.com>
2026-08-13 07:55:11 +0200

    Provide manual sealed price history app-wide

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,20 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { CollectionProvider } from '../state/collection';
+import { SealedPriceProvider } from '../store/sealedPrices';
 
 export default function RootLayout() {
   return (
     <CollectionProvider>
-      <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#080D19' } }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="item/[id]" />
-        <Stack.Screen name="card/[id]" />
-      </Stack>
+      <SealedPriceProvider>
+        <StatusBar style="light" />
+        <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#080D19' } }}>
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="item/[id]" />
+          <Stack.Screen name="card/[id]" />
+          <Stack.Screen name="sealed-price/[id]" />
+        </Stack>
+      </SealedPriceProvider>
     </CollectionProvider>
   );
 }
