4e24a36753a147b21a9429bca488bb10a35f5cf6
wellmannsdaniel99-hub <wellmanns.daniel99@gmail.com>
2026-08-13 08:00:36 +0200

    Provide collection and wishlist stores

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,22 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { CollectionProvider } from '../state/collection';
+import { CollectionProvider } from '../store/collection';
 import { SealedPriceProvider } from '../store/sealedPrices';
+import { WishlistProvider } from '../store/wishlist';
 
 export default function RootLayout() {
   return (
     <CollectionProvider>
       <SealedPriceProvider>
-        <StatusBar style="light" />
-        <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#080D19' } }}>
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="item/[id]" />
-          <Stack.Screen name="card/[id]" />
-          <Stack.Screen name="sealed-price/[id]" />
-        </Stack>
+        <WishlistProvider>
+          <StatusBar style="light" />
+          <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#080D19' } }}>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="item/[id]" />
+            <Stack.Screen name="card/[id]" />
+            <Stack.Screen name="sealed-price/[id]" />
+          </Stack>
+        </WishlistProvider>
       </SealedPriceProvider>
     </CollectionProvider>
   );
