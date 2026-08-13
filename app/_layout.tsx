3629b1a63806e87bd92585e0a6393ab80f8b712e
wellmannsdaniel99-hub <wellmanns.daniel99@gmail.com>
2026-08-13 07:44:03 +0200

    Register live card detail route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { CollectionProvider } from '../store/collection';
+import { CollectionProvider } from '../state/collection';
 
 export default function RootLayout() {
   return (
     <CollectionProvider>
       <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#080D19' } }} />
+      <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#080D19' } }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="item/[id]" />
+        <Stack.Screen name="card/[id]" />
+      </Stack>
     </CollectionProvider>
   );
 }
