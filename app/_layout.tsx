d83c90c852e2e8faa9c45047b4c72c9ea0a665dd
wellmannsdaniel99-hub <wellmanns.daniel99@gmail.com>
2026-08-13 07:41:17 +0200

    Provide collection state across app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { CollectionProvider } from '../store/collection';
 
 export default function RootLayout() {
   return (
-    <>
+    <CollectionProvider>
       <StatusBar style="light" />
       <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: '#080D19' } }} />
-    </>
+    </CollectionProvider>
   );
 }
